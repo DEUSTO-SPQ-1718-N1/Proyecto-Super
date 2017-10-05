9eb2977440988633348c6365ad2a8c408ba48c24
mejiares@opendeusto.es <mejiares>
2017-10-05 20:25:14 +0200

    Proyecto-Super

---
+++ src/main/java/com/deusto/SPQ1718/Proyecto_Super/App.java
@@ -0,0 +1,13 @@
+package com.deusto.SPQ1718.Proyecto_Super;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
