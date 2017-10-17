2aa2aeec742d2b381f8642897647335e2d2abda0
mejiares@opendeusto.es <mejiares>
2017-10-17 21:23:22 +0200

    Creación de clase y primeros métodos #DSPQ1718P1-18 add work 2h Development

---
+++ src/main/java/com/deusto/SPQ1718/DSPQ1718P1_18/AccesoEstadisticas.java
@@ -0,0 +1,24 @@
+package com.deusto.SPQ1718.DSPQ1718P1_18;
+
+public class AccesoEstadisticas {
+	
+	//crear tabla ternaria con Cliente-Producto-Empleado para ticket
+	
+	public int getResumenProducto(String p){
+		//SQL get producto where nombre = p
+		return 5;
+	}
+	
+	public String getProductoMasVendido(){
+		return "p";
+	}
+	
+	public String getEmpleadoMasVentas(){
+		return "e";
+	}
+	
+	
+	
+	
+
+}
