9cf89e17735a855cad44d0a8c2d97f6330ccd3e3
juncalcas <juncalcasadevante@opendeusto.es>
2017-10-17 18:51:48 +0200

    Adding other new class #DSPQ1718P1-32 add work 1h Development

---
+++ src/main/java/com/deusto/SPQ1718/DSPQ1718P1_32/EnviarOferta.java
@@ -0,0 +1,20 @@
+package com.deusto.SPQ1718.DSPQ1718P1_32;
+
+public class EnviarOferta {
+	
+	public void Aviso(){
+		//envia notificacion de que hay una nueva oferta
+		//se carga la ofera
+		
+		System.out.println("Hay una oferta disponible!");
+	}
+	
+	public void Oferta(){
+		
+		String producto ="";
+		int precio = 0;
+		
+		System.out.println ("El producto" + producto + " está de oferta! Consiguelo por SOLO" + precio + " euros!!");
+	}
+
+}
