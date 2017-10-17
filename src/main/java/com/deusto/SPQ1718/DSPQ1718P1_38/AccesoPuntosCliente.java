02e4a3f782951696f0a8118182e0219673c03bbd
mejiares@opendeusto.es <mejiares>
2017-10-17 21:28:09 +0200

    Creación de clase y primeros métodos #DSPQ1718P1-38 add work 2h Development

---
+++ src/main/java/com/deusto/SPQ1718/DSPQ1718P1_38/AccesoPuntosCliente.java
@@ -0,0 +1,17 @@
+package com.deusto.SPQ1718.DSPQ1718P1_38;
+
+public class AccesoPuntosCliente {
+	
+	public void getProductoMaComprado(){
+		
+	}
+	
+	public int getPuntos(){
+		return 0;
+	}
+	
+	public int getUltimaCompra(){
+		return 0;
+	}
+
+}
