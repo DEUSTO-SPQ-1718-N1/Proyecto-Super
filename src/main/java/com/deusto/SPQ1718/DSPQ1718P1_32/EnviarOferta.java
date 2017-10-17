76ce2a92d6937789ab3562b8e62e90f75955045d
juncalcas <juncalcasadevante@opendeusto.es>
2017-10-17 19:00:13 +0200

    Añadir nueva funcionalidad #DSPQ1718P1-32 add work 1h Development

--- src/main/java/com/deusto/SPQ1718/DSPQ1718P1_32/EnviarOferta.java
+++ src/main/java/com/deusto/SPQ1718/DSPQ1718P1_32/EnviarOferta.java
@@ -6,5 +6,10 @@ public class EnviarOferta {
 		
 		System.out.println("Hay una oferta disponible!");
 	}
-
+	
+	public void Oferta(){
+		String product="";
+		int precio =0;
+		System.out.println("El producto " + product + " esta de OFERTA. Consiguelos por SOLO " + precio +" euros");
+	}
 }
