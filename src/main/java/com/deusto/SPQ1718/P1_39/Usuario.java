a56217b1e4eb277ce058491cf969246e5ce53224
aitor-delgado <aitor.delgado@opendeusto.es>
2017-10-18 12:15:42 +0200

    Crear clase usuario

---
+++ src/main/java/com/deusto/SPQ1718/P1_39/Usuario.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package com.deusto.SPQ1718.P1_39;
+
+/**
+ * @author ALUMNO
+ *
+ */
+public class Usuario {
+
+	/**
+	 * 
+	 */
+	public Usuario() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
