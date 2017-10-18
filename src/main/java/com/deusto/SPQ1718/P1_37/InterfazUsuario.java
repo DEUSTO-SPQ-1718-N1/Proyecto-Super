4c906abb439b538a6601dd300c08e72848c9bfab
aitor-delgado <aitor.delgado@opendeusto.es>
2017-10-18 12:08:27 +0200

    Crear clase InterfazUsuario

---
+++ src/main/java/com/deusto/SPQ1718/P1_37/InterfazUsuario.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package com.deusto.SPQ1718.P1_37;
+
+/**
+ * @author ALUMNO
+ *
+ */
+public class InterfazUsuario {
+
+	/**
+	 * 
+	 */
+	public InterfazUsuario() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
