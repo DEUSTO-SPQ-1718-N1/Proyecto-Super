ac7b6df98dd9c81f4e7e48ac0461bc0a74d493a4
aitor-delgado <aitor.delgado@opendeusto.es>
2017-10-19 19:16:01 +0200

    Ajuste de rama

--- src/main/java/com/deusto/SPQ1718/Proyecto_Super/App.java
+++ src/main/java/com/deusto/SPQ1718/Proyecto_Super/App.java
@@ -1,13 +1,18 @@
 package com.deusto.SPQ1718.Proyecto_Super;
 
+import javax.swing.ImageIcon;
+
+import com.deusto.SPQ1718.P1_37.InterfazUsuario;
+import com.deusto.SPQ1718.P1_39.Usuario;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hola Mundo!" );
+	public static void main( String[] args )
+    {        
+    	Usuario miUsuario = new Usuario("Jessica", "Sanchez", "Perez", 1454, "src/main/java/com/deusto/SPQ1718/P1_37/img1.jpg");    	        
     }
 }
