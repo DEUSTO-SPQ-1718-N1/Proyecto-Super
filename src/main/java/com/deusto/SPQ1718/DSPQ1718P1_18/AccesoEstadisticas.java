1a247e7f395df769c4097338bafe332ac5dab1d9
mejiares@opendeusto.es <mejiares>
2017-10-19 22:47:05 +0200

    Creación de consultas #DSPQ1718P1-18 add work 6h Development

--- src/main/java/com/deusto/SPQ1718/DSPQ1718P1_18/AccesoEstadisticas.java
+++ src/main/java/com/deusto/SPQ1718/DSPQ1718P1_18/AccesoEstadisticas.java
@@ -1,20 +1,48 @@
 package com.deusto.SPQ1718.DSPQ1718P1_18;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class AccesoEstadisticas {
+
+	Connection conn;
+	/*Contexión a la BD*/
+	public AccesoEstadisticas() throws SQLException {
+		String s = System.getProperty("user.dir");
+	    String dbUrl = "jdbc:derby:"+s+"\\src\\main\\sql\\base;create=true";
+	    conn = DriverManager.getConnection(dbUrl);
+	  }
 	
-	//crear tabla ternaria con Cliente-Producto-Empleado para ticket
-	
-	public int getResumenProducto(String p){
-		//SQL get producto where nombre = p
-		return 5;
+	public ResultSet getResumenProducto(int p) throws SQLException{
+		/*Cuanto se ha comprado de un producto segun su id*/
+		 Statement stmt = conn.createStatement();
+		 ResultSet rs = stmt.executeQuery("SELECT * FROM producto WHERE id="+p);
+		return rs;
 	}
 	
-	public String getProductoMasVendido(){
-		return "p";
+	public ResultSet getProductoMasVendido() throws SQLException{
+		/*Que producto se ha vendido más*/
+		Statement stmt = conn.createStatement();
+		ResultSet rs = stmt.executeQuery("SELECT * FROM ( SELECT producto, COUNT(producto) cont FROM compra GROUP BY producto ) as calc1 WHERE (SELECT MAX(cont) m FROM ( SELECT COUNT(producto) cont FROM compra GROUP BY producto ) AS cacl) = cont");
+		return rs;
 	}
 	
-	public String getEmpleadoMasVentas(){
-		return "e";
+	public ResultSet getEmpleadoMasVentas() throws SQLException{
+		/*Que producto se ha vendido más*/
+		Statement stmt = conn.createStatement();
+		ResultSet rs = stmt.executeQuery("SELECT * FROM ( SELECT empleado, COUNT(empleado) cont FROM compra GROUP BY empleado ) as calc1 WHERE (SELECT MAX(cont) m FROM ( SELECT COUNT(empleado) cont FROM compra GROUP BY empleado ) AS cacl) = cont");
+		return rs;
+	}
+	
+	public ResultSet getExperincia(int c, int e) throws SQLException{
+		
+		Statement stmt = conn.createStatement();
+		 ResultSet rs = stmt.executeQuery("SELECT * FROM experiencia WHERE cliente="+c+" AND empleado ="+e );
+		return rs;
+		
 	}
 	
 	
