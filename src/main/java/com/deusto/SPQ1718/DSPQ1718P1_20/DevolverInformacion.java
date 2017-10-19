79a9589dde8a454eff36b59c2513d1113c7c197c
Paula <paulaurteaga@opendeusto.es>
2017-10-19 17:04:43 +0200

    Construccion de metodos para recoger datos de la BD y enviarlos a la interfaz

--- src/main/java/com/deusto/SPQ1718/DSPQ1718P1_20/DevolverInformacion.java
+++ src/main/java/com/deusto/SPQ1718/DSPQ1718P1_20/DevolverInformacion.java
@@ -1,8 +1,21 @@
 package com.deusto.SPQ1718.DSPQ1718P1_20;
 
 public class DevolverInformacion {
-	public int devolver(){
-		return 0;
+	//Inicializar variable de tipo BD
+	// BD bd= new BD();
+	//Object[][] data= new Object[][];
+	
+	
+	public  static void conexionBD(){
+		// bd.startConexion();
+		// data=bd.loadEstadisticas();
+		//pasarDatosInterfaz(data);
+		
+	}
+	public static void pasarDatosInterfaz(Object[][] data){
+		//this.data=data;
+		//InterfazUsuario interfaz= new InterfazUsuario(data);
+	    //interfaz.setVisible(true);
 	}
 
 }
