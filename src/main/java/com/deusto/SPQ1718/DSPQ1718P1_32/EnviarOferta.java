f9ae6e6b082a1d0e715b9d1dc9fb446a436e0f63
juncalcas <juncalcasadevante@opendeusto.es>
2017-10-19 12:43:33 +0200

    Añadir nuevos metodos #DSPQ1718P1-32 add work 1h Development

--- src/main/java/com/deusto/SPQ1718/DSPQ1718P1_32/EnviarOferta.java
+++ src/main/java/com/deusto/SPQ1718/DSPQ1718P1_32/EnviarOferta.java
@@ -1,15 +1,38 @@
 package com.deusto.SPQ1718.DSPQ1718P1_32;
 
+
 public class EnviarOferta {
-	
-	public void Aviso(){
+
+	private String aviso;
+	private String oferta;
+
+	public void aviso(){
 		
 		System.out.println("Hay una oferta disponible!");
+		setAviso("Hay una oferta disponible!");
 	}
 	
-	public void Oferta(){
+	public void oferta(){
 		String product="";
 		int precio =0;
+		
+		setOferta("El producto " + product + " esta de OFERTA. Consiguelos por SOLO " + precio +" euros");
 		System.out.println("El producto " + product + " esta de OFERTA. Consiguelos por SOLO " + precio +" euros");
+	}
+
+	public String getAviso() {
+		return aviso;
+	}
+
+	public void setAviso(String aviso) {
+		this.aviso = aviso;
+	}
+
+	public String getOferta() {
+		return oferta;
+	}
+
+	public void setOferta(String oferta) {
+		this.oferta = oferta;
 	}
 }
