c5aa5259a8bb0c98d4f6131dc05b6d81360a6395
juncalcas <juncalcasadevante@opendeusto.es>
2017-10-20 13:39:32 +0200

    Añadir funcion de visibilidad #DSPQ1718P1-32

--- src/main/java/com/deusto/SPQ1718/DSPQ1718P1_32/EnviarOferta.java
+++ src/main/java/com/deusto/SPQ1718/DSPQ1718P1_32/EnviarOferta.java
@@ -1,23 +1,71 @@
 package com.deusto.SPQ1718.DSPQ1718P1_32;
 
+import java.awt.Font;
+import java.awt.GridLayout;
+
+import javax.swing.JFrame;
+import javax.swing.JTextArea;
 
 public class EnviarOferta {
-
+//Se muestra una oferta para el cliente
 	private String aviso;
 	private String oferta;
+	public EnviarOferta(){
+		 
+	}
+	/*public static void main(String []args){
+		  
+		 EnviarOferta  of = new EnviarOferta();
+		 String aviso= of.aviso();
+		 String oferta = of.oferta();
+		 
+		 of.lanzarVentana(aviso, oferta); 
+		 
+		}
+*/
+	public void lanzarVentana(String aviso, String oferta){
+		 String titulo = "Recomendaciones";
+		 JFrame ventanaM = new JFrame(titulo);
+		 JTextArea jtextarea1 = new JTextArea();
+		 JTextArea jtextarea2 = new JTextArea();
+		 
+		  
+	     ventanaM.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);//finaliza el programa cuando se da click en la X
+	     ventanaM.setVisible(true);//configurando visualización de la ventana        
+	     
+	     ventanaM.setSize(800,500); 
+	     ventanaM.setLocationRelativeTo(null);
+	     ventanaM.setResizable(false);
 
-	public void aviso(){
+	     
+	     ventanaM.setLayout(new GridLayout(2,1));
+	     ventanaM.add(jtextarea1);
+	     ventanaM.add(jtextarea2);
+	     
+	     jtextarea1.setText(aviso);
+	     jtextarea2.setText(oferta);
+	     
+	     jtextarea1.setFont(new Font("Serif", Font.BOLD, 20));
+	     jtextarea2.setFont(new Font("Serif", Font.BOLD, 15));
+	     
+	}
+
+	public String aviso(){
 		
 		System.out.println("Hay una oferta disponible!");
-		setAviso("Hay una oferta disponible!");
+		String aviso=("Hay una oferta disponible!");
+		
+		return aviso;
 	}
 	
-	public void oferta(){
-		String product="";
-		int precio =0;
+	public String oferta(){
+		String product="leche";
+		int precio = 10;
+	
 		
-		setOferta("El producto " + product + " esta de OFERTA. Consiguelos por SOLO " + precio +" euros");
-		System.out.println("El producto " + product + " esta de OFERTA. Consiguelos por SOLO " + precio +" euros");
+		String oferta = ("El producto " + product + " esta de OFERTA. Tiene un descuento de " + precio +" %");
+		System.out.println("El producto " + product + " esta de OFERTA. Tiene un descuento de " + precio +" %");
+	return oferta;
 	}
 
 	public String getAviso() {
