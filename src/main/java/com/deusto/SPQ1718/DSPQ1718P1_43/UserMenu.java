6ecaed8377e25cf3ae1491f739c2f0ed655f3ff7
aitor-delgado <aitor.delgado@opendeusto.es>
2017-10-31 09:48:39 +0100

    Enlace menu user con funcionalidades #DSPQ1718P1-44 add work 1h Development

--- src/main/java/com/deusto/SPQ1718/DSPQ1718P1_43/UserMenu.java
+++ src/main/java/com/deusto/SPQ1718/DSPQ1718P1_43/UserMenu.java
@@ -2,16 +2,20 @@ package com.deusto.SPQ1718.DSPQ1718P1_43;
 
 import java.awt.Color;
 import java.awt.EventQueue;
+
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import javax.swing.JButton;
 import javax.swing.JLabel;
+
 import com.deusto.SPQ1718.DSPQ1718P1_19.InterfazUsuario;
 import com.deusto.SPQ1718.DSPQ1718P1_32.EnviarOferta;
 import com.deusto.SPQ1718.DSPQ1718P1_33.EnviarRecomendacion;
 import com.deusto.SPQ1718.P1_39.Usuario;
+import com.deusto.SPQ1718.Proyecto_Super.Inicio;
+
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 
@@ -41,8 +45,8 @@ public class UserMenu extends JFrame {
 				EnviarOferta  of = new EnviarOferta();
 				String aviso= of.aviso();
 				String oferta = of.oferta(); 
-				of.lanzarVentanaOfertas(aviso, oferta);
-				frame.setVisible(false);				
+				of.lanzarVentanaOfertas(aviso, oferta);	
+				Inicio.frame.setVisible(false);
 			}
 		});
 		button_6.setBounds(47, 245, 187, 50);
@@ -55,35 +59,24 @@ public class UserMenu extends JFrame {
 				String aviso= re.avisoRecomendacion();
 				String recomendacion = re.productoRecomendado();
 				re.lanzarVentanaRecomendaciones(aviso, recomendacion); 
-				frame.setVisible(false);
+				Inicio.frame.setVisible(false);
 			}
 		});
-		button_5.setBounds(287, 367, 220, 50);
+		button_5.setBounds(150, 367, 243, 50);
 		contentPane.add(button_5);
 				
 		JButton button_1 = new JButton("Puntos acumulados");
 		button_1.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
 				new Usuario("Jessica", "Sanchez", "Perez", 1454, "src/main/java/com/deusto/SPQ1718/P1_37/img1.jpg", "Nesquick", "Pan baguette", "Tarta de manzana", "Balon de futbol", "Lavadora Siemens", 5, 1, 7, 3, 35, "12/09/2017", "12/09/2017", "12/09/2017", "04/09/2017", "04/09/2017");
-				frame.setVisible(false);
+				Inicio.frame.setVisible(false);
 			}
 		});
 		button_1.setBounds(287, 245, 220, 50);
 		contentPane.add(button_1);
 		
-		JButton button_2 = new JButton("Historial de actividad");
-		button_2.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
-		        InterfazUsuario interfaz= new InterfazUsuario();
-		        interfaz.setVisible(true);
-				frame.setVisible(false);
-			}
-		});
-		button_2.setBounds(47, 367, 187, 50);
-		contentPane.add(button_2);
-		
 		JButton button_3 = new JButton("Valorar una Compra");
-		button_3.setBounds(563, 367, 165, 50);
+		button_3.setBounds(473, 367, 165, 50);
 		contentPane.add(button_3);
 		
 		JButton button_4 = new JButton("Manual de usuario");
