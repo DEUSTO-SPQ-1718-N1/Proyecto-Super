c397697f0a93497ed9f661bf171de052bdeba726
aitor-delgado <aitor.delgado@opendeusto.es>
2017-10-31 09:49:58 +0100

    Enlace menu administrador con funcionalidades #DSPQ1718P1-45 add work 1h Development

--- src/main/java/com/deusto/SPQ1718/DSPQ1718P1_43/UserMenu.java
+++ src/main/java/com/deusto/SPQ1718/DSPQ1718P1_43/UserMenu.java
@@ -1,21 +1,16 @@
 package com.deusto.SPQ1718.DSPQ1718P1_43;
 
 import java.awt.Color;
-import java.awt.EventQueue;
-
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import javax.swing.JButton;
 import javax.swing.JLabel;
-
-import com.deusto.SPQ1718.DSPQ1718P1_19.InterfazUsuario;
 import com.deusto.SPQ1718.DSPQ1718P1_32.EnviarOferta;
 import com.deusto.SPQ1718.DSPQ1718P1_33.EnviarRecomendacion;
 import com.deusto.SPQ1718.P1_39.Usuario;
 import com.deusto.SPQ1718.Proyecto_Super.Inicio;
-
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 
