4b5dbeecf9dec471d798a37488fdac561173ea6a
Paula <paulaurteaga@opendeusto.es>
2017-10-31 08:57:46 +0100

    Creacion de nueva rama para la historia de usuario DSPQ1718P1-6, que contiene a su vez dos tareas  #DSPQ1718P1-6 add work 1h Development

---
+++ src/main/java/com/deusto/SQP1718/DSPQ1718P1_6/ValoracionPostCompra.java
@@ -0,0 +1,16 @@
+package com.deusto.SQP1718.DSPQ1718P1_6;
+
+import java.awt.EventQueue;
+
+import com.deusto.SPQ1718.Proyecto_Super.App;
+
+public class ValoracionPostCompra {
+	
+	public static void main(String[] args) {
+		
+	}
+	public ValoracionPostCompra(){
+		
+	}
+
+}
