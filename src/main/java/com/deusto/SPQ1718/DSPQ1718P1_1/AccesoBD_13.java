8940093d6001221bee0d90b9819d479444e09df7
mejiares@opendeusto.es <mejiares>
2017-11-09 23:06:50 +0100

    Corrección y completar métodos #DSPQ1718P1-13 add work 30m Development

--- src/main/java/com/deusto/SPQ1718/DSPQ1718P1_1/AccesoBD_13.java
+++ src/main/java/com/deusto/SPQ1718/DSPQ1718P1_1/AccesoBD_13.java
@@ -13,11 +13,77 @@ public class AccesoBD_13 {
 	/*
 	 * Método de conexión con la base de datos a la dirección especificada
 	 */
+	
+	/*public static void main(String[] args) throws SQLException {
+		AccesoBD_13 a = new AccesoBD_13();
+		a.aumentarAlmacen(1,5);
+	}*/
 	public AccesoBD_13() throws SQLException{
 		String env= "base";
 		String value = System.getenv(env);
 		String dbUrl = "jdbc:derby:"+value+"\\sql\\base;create=true";
 	    conn = DriverManager.getConnection(dbUrl);
+	}
+	
+	public int getPuntosP(int p){
+		 int c = 0;
+		try {
+			Statement stmt = conn.createStatement();
+			ResultSet rs = stmt.executeQuery("SELECT puntos FROM producto  WHERE id = "+p);
+			while (rs.next()){
+				 c = rs.getInt("puntos");
+			 }
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return c;
+	}
+	
+	
+	public int getCantidadP(int p){
+		 int c = 0;
+		try {
+			Statement stmt = conn.createStatement();
+			ResultSet rs = stmt.executeQuery("SELECT cantidad FROM producto  WHERE id = "+p);
+			while (rs.next()){
+				 c = rs.getInt("cantidad");
+			 }
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return c;
+	}
+	
+	public Float getPrecioP(int p){
+		 float pr = 0;
+		try {
+			Statement stmt = conn.createStatement();
+			ResultSet rs = stmt.executeQuery("SELECT precio FROM producto  WHERE id = "+p);
+			while (rs.next()){
+				 pr = rs.getFloat("precio");
+			 }
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return pr;
+	}
+	
+	public String getNombreP(int p){
+		 String n="";
+		try {
+			Statement stmt = conn.createStatement();
+			ResultSet rs = stmt.executeQuery("SELECT nombre FROM producto WHERE id = "+p);
+			while (rs.next()){
+				 n = rs.getString("nombre");
+			 }
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return n;
 	}
 	/*
 	 * Donde p es igual a la clave del producto a actualizar y c la cantidad que se va 
@@ -25,12 +91,16 @@ public class AccesoBD_13 {
 	 */
 	public int aumentarAlmacen(int p, int c){
 		 Statement stmt;
+		 int nuevac = 0;
 		 try {
 			stmt = conn.createStatement();
 			ResultSet rs = stmt.executeQuery("SELECT cantidad FROM producto WHERE id = "+p);
 			/*El código de retorno 1 indica que no se ha encontrado el porducto con id p*/
 			if(rs.next()==false){ return 1;}
-			int nuevac = rs.getInt("cantidad") + c;
+			nuevac = rs.getInt("cantidad");
+			 System.out.println(nuevac);
+			 nuevac = nuevac + c;
+			 System.out.println(nuevac);
 			stmt.executeUpdate("update producto set cantidad ="+nuevac+" WHERE id ="+p);
 		} 
 		catch (SQLException e) {
@@ -47,6 +117,7 @@ public class AccesoBD_13 {
 	 */
 	public int disminuirAlmacen(int p, int c){
 		 Statement stmt;
+		 int nuevac = 0;
 		try {
 			
 			stmt = conn.createStatement();
@@ -54,7 +125,7 @@ public class AccesoBD_13 {
 			/*El código de retorno 1 indica que no se ha encontrado el porducto con id p*/
 			if(rs.next()==false){ return 1;}
 			/*El código de retorno 2 indica que de actualizarse la cantidad en almacen sería negativa, por tanto la operación no se puede hacer*/
-			int nuevac = rs.getInt("cantidad") - c;
+			nuevac = rs.getInt("cantidad") - c;
 			if(nuevac<0){return 2;}
 			stmt.executeUpdate("update producto set cantidad ="+nuevac+" WHERE id ="+p);
 		} 
@@ -140,14 +211,15 @@ public class AccesoBD_13 {
 	 * en almacen de c, el statement se hace de tipo prepeared, con el objetivo de evitar
 	 * inyección SQL en la inserción de Strings
 	 */
-	public int insertarProducto(String n, float p, int ca){
+	public int insertarProducto(String n, float p, int ca, int pu){
 		PreparedStatement stmt;
 		try {
 			
-			stmt = conn.prepareStatement("insert into producto(nombre , precio, cantidad) values (?,?,?)");
+			stmt = conn.prepareStatement("insert into producto(nombre , precio, cantidad, puntos) values (?,?,?,?)");
 			stmt.setString(1, n);
 			stmt.setFloat(2, p);
-			stmt.setInt(2, ca);
+			stmt.setInt(3, ca);
+			stmt.setInt(4, pu);
 			stmt.executeUpdate();
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
