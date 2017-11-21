0fde606ccc03f4e8417a455e7a81ee1dd8145c04
Paula <paulaurteaga@opendeusto.es>
2017-11-21 17:44:57 +0100

    Creacion de dos clases, una para la interfaz de usuario y la otra para conectar con la base de datos y hacer dos llamadas: recoger comentarios y media de puntuaciones #DSPQ1718P1-31 add work 2h Development

--- src/main/java/com/deusto/SPQ1718/DSPQ1718P1_8/ConexionBaseDatos.java
+++ src/main/java/com/deusto/SPQ1718/DSPQ1718P1_8/ConexionBaseDatos.java
@@ -1,5 +1,70 @@
 package com.deusto.SPQ1718.DSPQ1718P1_8;
 
-public class ConexionBaseDatos {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
 
+/*! \ class ConexionBaseDatos "paquete/"
+ *  \ Esta clase hace cosas muy guays
+ *  
+ *  Para eso hacemos una conexión bcidsbhfshdbu
+ */
+
+public class ConexionBaseDatos {
+	Connection conn;
+	public static void main(String[] args) throws SQLException {
+		ArrayList<String> e;
+		ConexionBaseDatos a = new ConexionBaseDatos();
+		e= a.getComentarios(1);
+		for (int i=0;i<e.size();i++){
+			System.out.println(e.get(i));
+		}
+	}
+	public ConexionBaseDatos() throws SQLException{
+		String env= "base";
+		String value = System.getenv(env);
+		String dbUrl = "jdbc:derby:"+value+"\\sql\\base;create=true";
+	    conn = DriverManager.getConnection(dbUrl);
+	}
+	/**
+	 * @param int que representa el id que tiene el cliente del que se desea consultar las opiniones
+	 * @return lista de comentarios que haya realizado el usuario con id id
+	 */
+	public ArrayList<String> getComentarios(int id){
+		ArrayList<String> comentarios= new ArrayList<String>();
+		try {
+			Statement stmt = conn.createStatement();/**< Esta variable permite utilizar la conexión de BD creada en el constructor para crear un statement SQL */
+			ResultSet rs = stmt.executeQuery("SELECT * FROM experiencia  WHERE cliente = "+id);
+			while (rs.next()){
+				 comentarios.add(rs.getString("comentario"));
+			 }
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			
+			e.printStackTrace();
+		}
+		return comentarios;
+	}
+    public int getMedia(int id){
+    	ArrayList<Integer> puntos= new ArrayList<Integer>();
+    	int suma=0;
+    	int coef =0;
+    	try {
+			Statement stmt = conn.createStatement();
+			ResultSet rs = stmt.executeQuery("SELECT * FROM experiencia  WHERE cliente = "+id);
+			while (rs.next()){
+				suma = suma + rs.getInt("experiencia");
+				coef++;
+			 }
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			
+			e.printStackTrace();
+		}
+    	
+    	return suma/coef;
+    	    }
 }
--- src/main/java/com/deusto/SPQ1718/DSPQ1718P1_8/InterfazUsuario.java
+++ src/main/java/com/deusto/SPQ1718/DSPQ1718P1_8/InterfazUsuario.java
@@ -1,5 +1,84 @@
 package com.deusto.SPQ1718.DSPQ1718P1_8;
 
-public class InterfazUsuario {
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.sql.SQLException;
+import java.util.ArrayList;
 
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
+
+import com.deusto.SPQ1718.DSPQ1718P1_43.UserMenu;
+import com.deusto.SPQ1718.DSPQ1718P1_6.VisualizacionValoracion;
+import com.deusto.SPQ1718.DSPQ1718P1_6.db;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import java.awt.Font;
+
+public class InterfazUsuario extends JFrame{
+	public ConexionBaseDatos basedatos;
+	private static InterfazUsuario inter;
+	private JTable table;
+	private ArrayList<String> comentarios;
+	private JTextField textField;
+	public static void main(String[] args) throws SQLException{
+		inter= new InterfazUsuario();
+		inter.setVisible(true);
+
+		
+	}
+	public  InterfazUsuario() throws SQLException{
+		basedatos= new ConexionBaseDatos();
+		this.setTitle("Visualizacion comentarios");
+		this.setSize(600, 437);
+		this.setLocationRelativeTo(null);
+		getContentPane().setLayout(null);
+		JPanel panel = new JPanel();
+		panel.setBounds(0, 0, 584, 398);
+		getContentPane().add(panel);
+		panel.setLayout(null);
+		JScrollPane scrollPane = new JScrollPane();
+		table = new JTable();
+		table.setBounds(10, 33, 564, 288);
+		panel.add(table);
+		int id=1;
+		DefaultTableModel datos = new DefaultTableModel();
+		datos.addColumn("Comentario");	
+		comentarios = basedatos.getComentarios(id);
+		Object [] row = new Object[comentarios.size()];
+		for(int i=0;i< comentarios.size();i++)
+		{
+			row[i]=comentarios.get(i);
+			//System.out.println(comentarios.get(i));
+		}
+		datos.addRow(row);
+		table.setModel(datos);
+		table.add(scrollPane);
+		JButton btnNewButton = new JButton("V O L V E R");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				UserMenu frame = new UserMenu();
+				frame.setVisible(true);	
+				dispose();
+			}
+		});
+		btnNewButton.setBounds(209, 344, 131, 43);
+		panel.add(btnNewButton);
+		
+		JLabel lblPuntuacionMedia = new JLabel("Puntuacion Media");
+		lblPuntuacionMedia.setFont(new Font("Tahoma", Font.BOLD, 11));
+		lblPuntuacionMedia.setBounds(350, 355, 112, 17);
+		panel.add(lblPuntuacionMedia);
+		
+		textField = new JTextField();
+		textField.setBounds(472, 355, 86, 20);
+		panel.add(textField);
+		textField.setColumns(10);
+		textField.setText(Integer.toString(basedatos.getMedia(1)));
+		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+	}
 }
