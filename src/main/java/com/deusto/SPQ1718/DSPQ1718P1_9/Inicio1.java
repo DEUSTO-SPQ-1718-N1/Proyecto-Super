fd073e9ce21d618988581d7c5ca830ce00835d61
aitor-delgado <aitor.delgado@opendeusto.es>
2017-11-28 17:26:31 +0100

    faltaba subir main #DSPQ1718P1-48

--- src/main/java/com/deusto/SPQ1718/DSPQ1718P1_9/Inicio1.java
+++ src/main/java/com/deusto/SPQ1718/DSPQ1718P1_9/Inicio1.java
@@ -8,9 +8,6 @@ public class Inicio1 {
 	}
 
 	public static void main(String[] args) throws SQLException {
-		//AccesoBD_9 Prueba0 = new AccesoBD_9();
-		//String ResProd = Prueba0.getNombreP(1);	
-		//System.out.println(ResProd);
 
 		IEstadisticas frest1 = new IEstadisticas();
 		frest1.setVisible(true);		
